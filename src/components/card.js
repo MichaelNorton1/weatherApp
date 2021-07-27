bc9e4da3a258a6c59617a42974803fd7e7099ec4
Michael Norton <michael.norton1213@gmail.com>
2021-07-27 16:57:13 -0400

    changed for mobile

--- src/components/card.js
+++ src/components/card.js
@@ -6,17 +6,21 @@ const Card = (props) => {
   return arr === undefined ? (
     <div className="tc">Please enter a valid ZipCode</div>
   ) : (
-    <div className=" center w-50 br4 db ba b--black-10 bg-gray">
-      <div className=" pl2 items-column">
-        <div className=" pr2 tc city washed-blue">{arr[1]}</div>
-        <div className="pl5">
-          <img
-            className=" pr4 pl4 icon bg-black br4"
-            alt=""
-            src={`http://openweathermap.org/img/wn/${arr[2]}@2x.png`}
-          ></img>
+    <div>
+      <div className="br2  dark-gray  mv5 w-100 w-50-m w-40-l mw5 center">
+        <img
+          src={`http://openweathermap.org/img/wn/${arr[2]}@2x.png`}
+          className="db center bg-black w-50 br4 br"
+          alt="weather."
+        />
+        <div className="pa2 ph2-ns pb3-ns">
+          <div className="dt w-100 mt1">
+            <div className="dtc ">
+              <h1 className="f5 f4-ns mv0 bg-light-gray br4">{arr[1]}</h1>
+            </div>
+          </div>
+          <p className="  f6 lh-copy measure mt2 ">{arr[0]} degrees</p>
         </div>
-        <div className=" pr2 tc temp washed-blue">{arr[0]} degrees</div>
       </div>
     </div>
   );
